6b6fed1e8a8bbd381be2e23c9827b365fb13ac89
taiki.sakaguchi <taiki.sakaguchi826@gmail.com>
2020-09-24 11:12:28 +0000

    correction

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,10 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
   before_action :require_user_logged_in
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
+  
   
   def index
-    if logged_in?
-      @task = current_user.tasks.build
       @tasks = current_user.tasks.order(id: :desc)
-    end
   end
 
   def show
@@ -52,8 +50,11 @@ class TasksController < ApplicationController
   
   private
   
-  def set_task
-    @task = current_user.tasks.find(params[:id])
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
   
   def task_params
